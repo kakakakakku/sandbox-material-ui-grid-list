1cb8d19b17aaa0c964cb43228637c98845f7bd14
kakakakakku <y.yoshida22@gmail.com>
2020-01-12 01:19:05 +0900

    Added GridList and GridListTile

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+import cat from './images/cat.jpg';
+import deer from './images/deer.jpg';
+import kingfisher from './images/kingfisher.jpg';
+import koala from './images/koala.jpg';
+import pelikan from './images/pelikan.jpg';
+import rabbit from './images/rabbit.jpg';
+import tiger from './images/tiger.jpg';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500
+    },
+  }),
+);
+
 const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <GridList cellHeight={200} className={classes.gridList} cols={3}>
+        <GridListTile key="cat" cols="2">
+          <img src={cat} alt="cat" />
+        </GridListTile>
+        <GridListTile key="deer" cols="1">
+          <img src={deer} alt="deer" />
+        </GridListTile>
+        <GridListTile key="kingfisher" cols="1">
+          <img src={kingfisher} alt="kingfisher" />
+        </GridListTile>
+        <GridListTile key="koala" cols="1">
+          <img src={koala} alt="koala" />
+        </GridListTile>
+        <GridListTile key="pelikan" cols="1">
+          <img src={pelikan} alt="pelikan" />
+        </GridListTile>
+        <GridListTile key="rabbit" cols="1">
+          <img src={rabbit} alt="rabbit" />
+        </GridListTile>
+        <GridListTile key="tiger" cols="2">
+          <img src={tiger} alt="tiger" />
+        </GridListTile>
+      </GridList>
     </div>
   );
 }
